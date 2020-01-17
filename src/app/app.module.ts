971aee51a5394c9e07a5e2a5453e71eb1453e5f0
hashem-buzer <hbuzer98@gmail.com>
2020-01-17 15:18:30 +0100

    push to dhia pulls

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,25 +6,35 @@ import { AppComponent } from "./app.component";
 
 import { FormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { Routes ,RouterModule } from '@angular/router';
-import { CustomerPageComponent } from './pages/customer-page/customer-page.component';
-import { MainPageComponent } from './main-page/main-page.component';
-// import { CategoriesBtnsComponent } from './components/';
+import { NavbarComponent } from "./components/navbar/navbar.component";
+import { FooterComponent } from "./components/footer/footer.component";
+import { Routes, RouterModule } from "@angular/router";
+import { CustomerPageComponent } from "./pages/customer-page/customer-page.component";
+import { MainPageComponent } from "./main-page/main-page.component";
+import { CategoriesBtnsComponent } from "../app/components/categories-btns/categories-btns.component";
 
-const routes: Routes =[
-  {path: '', component: MainPageComponent},
-  {path: 'user', component: CustomerPageComponent}
-]
+const routes: Routes = [
+  { path: "", component: MainPageComponent },
+  { path: "user", component: CustomerPageComponent }
+];
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, FooterComponent, CustomerPageComponent, MainPageComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule,
-     RouterModule.forRoot(routes)
-],
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    CustomerPageComponent,
+    MainPageComponent,
+    CategoriesBtnsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes)
+  ],
   providers: [],
-  bootstrap: [AppComponent],
-  
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
