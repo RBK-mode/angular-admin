a4b129680ea303d5577e049ebb67956e1275b9f0
Dhia Raissi <estraissi@gmail.com>
2020-01-17 11:14:34 +0100

    push to pull

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CustomerPageComponent } from './pages/customer-page/customer-page.component';
+import { OrdersPageComponent } from './pages/orders-page/orders-page.component';
+import { ApprovalPageComponent } from './pages/approval-page/approval-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomerPageComponent,
+    OrdersPageComponent,
+    ApprovalPageComponent
   ],
   imports: [
     BrowserModule,
