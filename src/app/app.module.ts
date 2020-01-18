4d530b2c88499570adc792a211cb7c24252f9baa
hashem-buzer <hbuzer98@gmail.com>
2020-01-18 20:56:39 +0100

    meged and testing Omar push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,33 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { CategoriesListComponent } from './components/categories-list/categories-list.component';
-import { CategoriesItemComponent } from './components/categories-list/categories-item/categories-item.component';
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule , ReactiveFormsModule} from "@angular/forms";
-import { ItemsListComponent } from './items-list/items-list.component';
-import { ItemComponent } from './items-list/item/item.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { CategoriesListComponent } from "./components/categories-list/categories-list.component";
+import { CategoriesItemComponent } from "./components/categories-list/categories-item/categories-item.component";
+import { HttpClientModule } from "@angular/common/http";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { ItemsListComponent } from "./items-list/items-list.component";
+import { ItemComponent } from "./items-list/item/item.component";
+import { Routes } from "@angular/router";
+import { MainPageComponent } from "./pages/main-page/main-page.component";
+import { LoginComponent } from "./pages/login/login.component";
+import { CustomerPageComponent } from "./pages/customer-page/customer-page.component";
+import { ApprovalPageComponent } from "./pages/approval-page/approval-page.component";
+import { OrdersPageComponent } from "./pages/orders-page/orders-page.component";
+import { DeninedPageComponent } from "./pages/denined-page/denined-page.component";
+
+const routes: Routes = [
+  { path: "", component: MainPageComponent },
+  { path: "login", component: LoginComponent },
+  { path: "user", component: CustomerPageComponent },
+  { path: "requests", component: ApprovalPageComponent },
+  { path: "items", component: ItemsListComponent, data: { form: false } },
+  { path: "addItem", component: ItemsListComponent, data: { form: true } },
+  { path: "orders", component: OrdersPageComponent },
+  { path: "approved", component: ApprovalPageComponent },
+  { path: "denied", component: DeninedPageComponent }
+];
 
 @NgModule({
   declarations: [
@@ -16,7 +35,7 @@ import { ItemComponent } from './items-list/item/item.component';
     CategoriesListComponent,
     CategoriesItemComponent,
     ItemsListComponent,
-    ItemComponent    
+    ItemComponent
   ],
   imports: [
     BrowserModule,
@@ -28,4 +47,4 @@ import { ItemComponent } from './items-list/item/item.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -5,10 +5,9 @@ import { HttpClient, HttpHeaders } from "@angular/common/http";
   providedIn: "root"
 })
 export class HttpService {
-
   constructor(private http: HttpClient) {}
 
-  getAdmin() {
+  getUsers() {
     return this.http.get("http://localhost:8000/api/user", {
       headers: new HttpHeaders({
         auth:
