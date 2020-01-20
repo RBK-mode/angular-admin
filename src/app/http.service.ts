b22cae219d64971038cb0409b59f9c51ed965a08
hashem-buzer <hbuzer98@gmail.com>
2020-01-20 12:57:35 +0100

    before pull from bara last commit

--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -11,47 +11,32 @@ export class HttpService {
   constructor(private http: HttpClient) {}
 
   getUsers() {
-    return this.http.get("http://localhost:8000/api/user", {
-      headers: new HttpHeaders({
-        auth:
-          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZTIwYTY4YTgyNWEyYzAzZjRhYmI5OWQiLCJpYXQiOjE1NzkzNTc5NTV9.hQrqmdssy7KN0FfJ8HjJp6IZd72xyPY-z9ai0v78m6U"
-      })
-    });
+    return this.http.get("http://localhost:8000/api/user");
   }
 
   getOrders() {
-    return this.http.get("http://localhost:8000/api/order", {
-      headers: new HttpHeaders({
-        auth:
-          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZTIwYTY4YTgyNWEyYzAzZjRhYmI5OWQiLCJpYXQiOjE1NzkzNTc5NTV9.hQrqmdssy7KN0FfJ8HjJp6IZd72xyPY-z9ai0v78m6U"
-      })
-    });
+    return this.http.get("http://localhost:8000/api/order");
   }
 
   getCats() {
-    return this.http.get("http://localhost:8000/api/category", {
-      headers: new HttpHeaders({
-        auth:
-          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZTIwYTY4YTgyNWEyYzAzZjRhYmI5OWQiLCJpYXQiOjE1NzkzNTc5NTV9.hQrqmdssy7KN0FfJ8HjJp6IZd72xyPY-z9ai0v78m6U"
-      })
-    });
+    return this.http.get("http://localhost:8000/api/category");
   }
 
   getCustomers() {
-    return this.http.get("http://localhost:8000/api/user", {
-      headers: new HttpHeaders({
-        auth:
-          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZTIwYTY4YTgyNWEyYzAzZjRhYmI5OWQiLCJpYXQiOjE1NzkzNTc5NTV9.hQrqmdssy7KN0FfJ8HjJp6IZd72xyPY-z9ai0v78m6U"
-      })
-    });
+    return this.http.get("http://localhost:8000/api/user");
   }
 
   getOrder() {
-    return this.http.get("http://localhost:8000/api/order", {
-      headers: new HttpHeaders({
-        auth:
-          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZTIwYTY4YTgyNWEyYzAzZjRhYmI5OWQiLCJpYXQiOjE1NzkzNTc5NTV9.hQrqmdssy7KN0FfJ8HjJp6IZd72xyPY-z9ai0v78m6U"
-      })
-    });
+    return this.http.get("http://localhost:8000/api/order");
+  }
+
+  httpOptions = { headers: new HttpHeaders() };
+
+  postOrderState(id) {
+    console.log(id);
+    return this.http.post(
+      `http://localhost:8000/api/order/reject/${id}`,
+      this.httpOptions
+    );
   }
 }
--- src/app/pages/orders-page/orders-page.component.ts
+++ src/app/pages/orders-page/orders-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { HttpService } from "./../../http.service";
 import { COMPOSITION_BUFFER_MODE } from "@angular/forms";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "app-orders-page",
@@ -10,13 +11,36 @@ import { COMPOSITION_BUFFER_MODE } from "@angular/forms";
 export class OrdersPageComponent implements OnInit {
   orders: any;
 
-  constructor(private _HttpService: HttpService) {}
+  constructor(private _HttpService: HttpService, private router: Router) {}
+
+  acceptOrder(id) {
+    console.log("accepted");
+    console.log(id);
+
+    this._HttpService.postOrderState(id).subscribe(data => {
+      this.router.navigate(["orders"]);
+      this.ngOnInit();
+      console.log(data);
+    });
+  }
+
+  rejectOrder(id) {
+    console.log(id);
+
+    this._HttpService.postOrderState(id).subscribe(data => {
+      this.router.navigate(["orders"]);
+      this.ngOnInit();
+      console.log(data);
+    });
+  }
 
   ngOnInit() {
     this._HttpService.getOrders().subscribe((data: Array<any>) => {
       this.orders = data;
-      console.log(this.orders);
-      console.log(this.orders[0].items_list[0].name);
+      console.log(data, "hiiiiiiiiiiiiiiiiiiiiiiiiiiiii");
+
+      // console.log(this.orders);
+      //console.log(this.orders[0].items_list[0].name);
     });
   }
 }
